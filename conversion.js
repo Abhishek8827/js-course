dfde96ba37275a07ccb92deb68f5f965590fb9ab
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-26 22:10:47 +0530

    comparison

--- conversion.js
+++ conversion.js
@@ -1,11 +1,13 @@
+//  *************************** Convertion ******************
+
 let height = "hitu";
 
-console.log(typeof height);
+// console.log(typeof height);
 
 let valueInNumber = Number(height);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
 // to number Number()
 // "33" => 33
 // "3a" => NaN
@@ -17,3 +19,31 @@ console.log(valueInNumber);
 // "Hello" -> true
 
 // to string String()
+// to bigint BigInt()
+
+// ************************** Operations ******************
+let value = 3;
+let nagValue = -value;
+// console.log(nagValue);
+
+let str1 = "Abhishek";
+let str2 = " Wani";
+const userName = str1 + str2;
+// console.log(userName);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 3);
+// console.log(1 + 2 + "3");
+
+let a = 5;
+let b = 5;
+
+let result = a++ + ++b;
+// 1. Postfix/Prefix execute first:
+//    a++ evaluates to 5 (but changes 'a' to 6 in memory)
+//    ++b changes 'b' to 6 and evaluates to 6
+// 2. Binary addition runs last:
+//    5 + 6 = 11
+
+console.log(result); // 11
