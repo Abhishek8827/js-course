3c6976580f35160e4590dcbf0232efc2f9f73763
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-26 22:36:09 +0530

    datatype

---
+++ dataTypes.js
@@ -0,0 +1,5 @@
+// primitive & non primitive
+
+// primitive 7 types => strings, number, boolean, null, undefined, bigInt, symbol
+
+// non primitive also reference type => objects, array, functions
