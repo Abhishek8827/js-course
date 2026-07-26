d6e1ef03288a4ef72f47bda01340621f710a7de8
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-26 22:27:24 +0530

    data types

---
+++ comparison.js
@@ -0,0 +1,19 @@
+// console.log(2 > 1);
+// console.log(2 < 1);
+// console.log(2 >= 1);
+// console.log(2 != 1);
+
+// console.log(2 > "1");
+// console.log("2" > 1);
+
+// avoid to use
+// The reason is that an equality check == and comparison > < > = <= works differently.
+// comparison convert null to a number, treating it is a 0
+// console.log(null > 0);
+// console.log(null == 0);
+// console.log(null >= 0);
+
+// console.log(undefined < 0);
+
+// strict check === check type and values.
+// console.log(5 === 5);
