adbbf87ad93ba2abade40ea9023453a2fa1f3183
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-26 11:55:49 +0530

    file rename

---
+++ first.js
@@ -0,0 +1,7 @@
+console.log("Hello Abhi...");
+let account = 9518;
+const userNAme = "Abhishek";
+var pass = 5561; //prefer not to use
+console.log(typeof account);
+
+console.table([account, userNAme, pass]);
