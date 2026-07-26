8977c226bf80c072e0590e0e038cff6216477588
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-26 14:03:31 +0530

    datatype file created

---
+++ datatype.js
@@ -0,0 +1,12 @@
+"use strict";
+let accountability = "is important";
+console.log(accountability);
+
+// number
+// string
+// bigint
+// boolean
+// undefined: not define any data in var
+// null: empty stored box
+// symbol
+// object
