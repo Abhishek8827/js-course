d78bcd9f9b16c59d0836cf416c25b5217f3c82c8
Abhishek8827 <01abhishekwani@gmail.com>
2026-07-28 00:34:50 +0530

    file update

--- 05_dataTypes.js
+++ 05_dataTypes.js
@@ -25,17 +25,21 @@ const dataTable = [
 
 const id = Symbol("1255");
 const pen = Symbol("1255");
+// both are diff even same value because we use symbol
 console.log(id === pen);
 
 // non primitive also preference type => objects, array, functions
 
+// array:
 const languages = ["js", "node", "react"];
 
+// object: in the pair of key and value
 let myObj = {
   name: "Abhishek",
   age: 22,
 };
 
+// funstion:
 const myFunction = function () {
   console.log("Hello World");
 };
