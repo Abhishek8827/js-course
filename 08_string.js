b7109c4b7a91975c2adbe2735ebe32110dc04242
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-05 23:23:38 +0530

    string methods

--- 08_string.js
+++ 08_string.js
@@ -9,5 +9,18 @@ console.log(`Hello my name is ${userName} ${lastName} and my age is: ${age}`); /
 
 // console.log("Freind's");
 
-const fatherName = new String("Rajesh");
-console.log(fatherName.toUpperCase);
+const fatherName = new String("  Rajesh    ");
+// console.log(fatherName.toUpperCase());
+// console.log(fatherName.charAt(2));
+// console.log(fatherName.indexOf("a"));
+
+// const newString = fatherName.slice(-4);
+// console.log(fatherName.trim()); // works on on whitespaces and also has trimEnd & trimStart
+
+const url = "https://google.com/javascript%20course";
+console.log(url.replace("%20", "-"));
+
+console.log(url.includes("google"));
+
+const movie = "RRR-Singham Krukishetra";
+console.log(movie.blink());
