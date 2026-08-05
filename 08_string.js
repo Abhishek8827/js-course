0b3fbce437210919fb912d7aae1a2dcc6aa32340
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-05 22:39:46 +0530

    strings

---
+++ 08_string.js
@@ -0,0 +1,13 @@
+// strings in js
+
+let userName = "Aman ";
+let lastName = " Ram";
+let age = 50;
+
+console.log(userName + age + lastName + " " + "Ok"); // not to use it was outdated
+console.log(`Hello my name is ${userName} ${lastName} and my age is: ${age}`); //best to use backticks as litereal, string interpolation, modern usage
+
+// console.log("Freind's");
+
+const fatherName = new String("Rajesh");
+console.log(fatherName);
