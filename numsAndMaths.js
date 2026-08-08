3583d1728dabce2dbfe0204440fde24b5746f42d
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-08 22:58:56 +0530

    arrays

---
+++ numsAndMaths.js
@@ -0,0 +1,9 @@
+const score = 450;
+
+// let grid = [["b", "c"]];
+// grid[0].unshift(["a", "abhi"]); // Target row 0, add to front
+// console.log(grid); // Output: [ ["a", "b", "c"] ]
+
+let grid = [["a", "b"]];
+grid.push("c"); // Adds a new row at the end
+console.log(grid); // Output: [ ["a", "b"], ["c", "d"] ]
