f18a3dccd0a34da364f4f49d6f45ef8eb2931828
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-08 23:02:17 +0530

    precision

--- numsAndMaths.js
+++ numsAndMaths.js
@@ -1,9 +1,11 @@
-const score = 450;
+const score = 450.526;
+
+console.log(score.toPrecision(3));
 
 // let grid = [["b", "c"]];
 // grid[0].unshift(["a", "abhi"]); // Target row 0, add to front
 // console.log(grid); // Output: [ ["a", "b", "c"] ]
 
-let grid = [["a", "b"]];
-grid.push("c"); // Adds a new row at the end
-console.log(grid); // Output: [ ["a", "b"], ["c", "d"] ]
+// let grid = [["a", "b"]];
+// grid.push("c"); // Adds a new row at the end
+// console.log(grid); // Output: [ ["a", "b"], ["c", "d"] ]
