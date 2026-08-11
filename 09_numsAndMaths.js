fe3cee375eb8aca24e6cf3e0fbff24301141e92f
Abhishek8827 <01abhishekwani@gmail.com>
2026-08-12 00:16:09 +0530

    add initial implementation of numsAndMaths.js with score examples

---
+++ 09_numsAndMaths.js
@@ -0,0 +1,21 @@
+const score = 450.05;
+// console.log(typeof score, score);
+// console.log(score.toString());
+
+const score1 = new Number(100); //creates a Number object rather than a primitive number value. Mostly Avoided
+// console.log(score1);
+
+// console.log(score.toPrecision(5));
+// console.log(score.toLocaleString("en-US"));
+// console.log(score.toString().length);
+// console.log(score.toFixed(1));
+
+// let grid = [["b", "c"]];
+// grid[0].unshift(["a", "abhi"]); // Target row 0, add to front
+// console.log(grid); // Output: [ ["a", "b", "c"] ]
+
+// let grid = [["a", "b"]];
+// grid.push("c"); // Adds a new row at the end
+// console.log(grid); // Output: [ ["a", "b"], ["c", "d"] ]
+
+// +++++++++++++++++++ Maths ++++++++++++++++
